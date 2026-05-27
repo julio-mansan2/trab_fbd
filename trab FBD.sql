f8b58f97bffb5c7c436e9097d2f56f24f090a13c
Júlio Mansan <julio.mansan31@gmail.com>
2026-05-27 00:11:03 -0300

    tabelas normalizadas (a princípio)

--- trab FBD.sql
+++ trab FBD.sql
@@ -1,16 +1,18 @@
 create database airbnb_db;
 
+-- Tabela Desnormalizada
+
 create table airbnb_desnormalizado (
-	id bigint,
-	location_name text,
+	id bigint primary key,
+	name text,
     host_id bigint,
     host_profile_id bigint,
-    host_name text,
-    neighbourhood_group text,
-    neighbourhood text,
+    host_name varchar(255),
+    neighbourhood_group varchar(255),
+    neighbourhood varchar(255),
     latitude double,
     longitude double,
-    room_type text,
+    room_type varchar(100),
     price double,
     minimum_nights int,
     number_of_reviews int,
@@ -22,6 +24,67 @@ create table airbnb_desnormalizado (
     license text
 );
 
-drop table location;
+drop table airbnb_desnormalizado;
+
+-- Tabelas Normalizadas (talvez)
+
+CREATE TABLE airbnb_host (
+    host_id BIGINT PRIMARY KEY,
+    host_profile_id BIGINT,
+    host_name VARCHAR(255)
+);
+
+CREATE TABLE neighbourhood_group (
+    neighbourhood_group_name VARCHAR(255) PRIMARY KEY
+);
+
+CREATE TABLE neighbourhood (
+    neighbourhood_name VARCHAR(255) PRIMARY KEY,
+    neighbourhood_group VARCHAR(255),
+
+    FOREIGN KEY(neighbourhood_group)
+        REFERENCES neighbourhood_group(neighbourhood_group_name)
+);
+
+CREATE TABLE room_type (
+    room_type VARCHAR(100) PRIMARY KEY
+);
+
+CREATE TABLE listing (
+    listing_id BIGINT PRIMARY KEY,
+    listing_name TEXT,
+
+    price DOUBLE,
+    minimum_nights INT,
+    availability_365 INT,
+    license TEXT,
+
+    latitude DOUBLE,
+    longitude DOUBLE,
+
+    neighbourhood_name VARCHAR(255),
+    room_type VARCHAR(100),
+    host_id BIGINT,
+
+    FOREIGN KEY(neighbourhood_name)
+        REFERENCES neighbourhood(neighbourhood_name),
+
+    FOREIGN KEY(room_type)
+        REFERENCES room_type(room_type),
+
+    FOREIGN KEY(host_id)
+        REFERENCES airbnb_host(host_id)
+);
+
+create table review_summary (
+	listing_id bigint primary key,
+    number_of_reviews int,
+    last_review date,
+    reviews_per_month double,
+    number_of_reviews_ltm int,
+    
+    foreign key(listing_id)
+		references listing(listing_id)
+);
 
 use airbnb_db
